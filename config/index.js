3f8b441fd356dcbdf4e7c06f540752bdeb090fd5
BensonYan <yanb25@mail2.sysu.edu.cn>
2019-04-15 17:38:51 +0800

    Update index.js
    update according to our config.

--- config/index.js
+++ config/index.js
@@ -1,32 +1,32 @@
 module.exports = {
     livenet: {
-        url: 'https://mainnet.infura.io',
+        url: 'http://localhost:8545',
         contractAddress: {},
     },
     livenet_private: {
-        url: 'https://sidechain.livenet.sonm.com',
+        url: 'http://localhost:7545',
         contractAddress: {
-            addressRegistry: '0xd1a6f3d1ae33b4b19565a6b283d7a05c5a0decb0',
+            addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
     },
     rinkeby: {
-        url: 'https://rinkeby.infura.io',
+        url: 'http://localhost:8545',
         contractAddress: {},
     },
     rinkeby_private: {
-        url: 'https://sidechain-dev.sonm.com',
+        url: 'http://localhost:7545',
         contractAddress: {
-            addressRegistry: '0x79b084653ca2588ed3915159e368db58aef165ee',
+            addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
     },
     testrpc: {
-        url: 'https://proxy.test.sonm.com:8545',
+        url: 'http://localhost:8545',
         contractAddress: {},
     },
     testrpc_private: {
-        url: 'https://proxy.test.sonm.com:8546',
+        url: 'http://localhost:7545',
         contractAddress: {
-            addressRegistry: '0xa78d434b49dd7267c88cbf5ed181598d8290006b',
+            addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
     },
 };
