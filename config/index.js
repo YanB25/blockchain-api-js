4c4f9e7ef38779180a04cef631570b0ab8fea6b7
BensonYan <yanb25@mail2.sysu.edu.cn>
2019-04-15 18:17:06 +0800

    Update index.js
    change localhost to server.bensyan.top

--- config/index.js
+++ config/index.js
@@ -1,30 +1,30 @@
 module.exports = {
     livenet: {
-        url: 'http://localhost:8545',
+        url: 'http://server.bensyan.top:8545',
         contractAddress: {},
     },
     livenet_private: {
-        url: 'http://localhost:7545',
+        url: 'http://server.bensyan.top:7545',
         contractAddress: {
             addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
     },
     rinkeby: {
-        url: 'http://localhost:8545',
+        url: 'http://server.bensyan.top:8545',
         contractAddress: {},
     },
     rinkeby_private: {
-        url: 'http://localhost:7545',
+        url: 'http://server.bensyan.top:7545',
         contractAddress: {
             addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
     },
     testrpc: {
-        url: 'http://localhost:8545',
+        url: 'http://server.bensyan.top:8545',
         contractAddress: {},
     },
     testrpc_private: {
-        url: 'http://localhost:7545',
+        url: 'http://server.bensyan.top:7545',
         contractAddress: {
             addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
