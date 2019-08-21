867b70ce4e724f0b47f4a3c864009e46daf3b282
yanb25 <yanb25@mail2.sysu.edu.cn>
2019-08-21 22:31:42 +0800

    change url to *.dds.sysu.tech

--- config/index.js
+++ config/index.js
@@ -1,30 +1,30 @@
 module.exports = {
     livenet: {
-        url: 'http://server.bensyan.top:8545',
+        url: 'http://mainchain.dds.sysu.tech',
         contractAddress: {},
     },
     livenet_private: {
-        url: 'http://server.bensyan.top:7545',
+        url: 'http://sidechain.dds.sysu.tech',
         contractAddress: {
             addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
     },
     rinkeby: {
-        url: 'http://server.bensyan.top:8545',
+        url: 'http://mainchain.dds.sysu.tech',
         contractAddress: {},
     },
     rinkeby_private: {
-        url: 'http://server.bensyan.top:7545',
+        url: 'http://sidechain.dds.sysu.tech',
         contractAddress: {
             addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
     },
     testrpc: {
-        url: 'http://server.bensyan.top:8545',
+        url: 'http://mainchain.dds.sysu.tech',
         contractAddress: {},
     },
     testrpc_private: {
-        url: 'http://server.bensyan.top:7545',
+        url: 'http://sidechain.dds.sysu.tech',
         contractAddress: {
             addressRegistry: '0x70a884df5916196775cb2ff6016e36a5ba30df92',
         },
